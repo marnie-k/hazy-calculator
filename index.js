f5ec7e6a8048f52893bc6e91cbb6937f6258e305
Marnie K <marnie.kelly.123@gmail.com>
2021-11-21 13:14:27 -0500

    more detailed pseudocode

--- index.js
+++ index.js
@@ -10,21 +10,46 @@ an  array that can contain numbers, strings, empty strings, null, zeros
 3. What is the output?
 a number or NaN
 
-    run for loop over arrays
+
+Function steps:
+
+1. Filter array
+      "The filter() method creates a new array filled with elements that 
+      pass a test provided by a function.
+
+      The filter() method does not execute the function for empty elements."    
+        this will take case of:
+        * `undefined` values should be ignored
+        * Empty string values (eg. `''`) should be ignored  
+        * Non-numeric values (eg. `'foo'`) should be ignored
+        *   remove empty values and non numeric strings as condition of filter
+          ???? will this remove operands?????
+
+2. Run for loop over new array
         convert stringified numbers to numbers -> push to new array
-        remove empty values and non numeric strings -> push to new array
+        * Stringified numbers (eg. `'2'`) should be treated as numbers
+           ???? is this necessary - maybe just use truthy ==  ?????
+           
         convert null to zero -> push to new array
-    
-    convert array to object
+        * `NULL` should be treated as zero -> push to new array
+  
+3. Convert new array to object
+    "The Object.assign() method is used to copy the values of all enumerable 
+    own properties from one or more source objects to a target object. 
+    It will return the target object.
+
+      Object.assign({}, ['a','b','c']); "
+
       key value pairs:
           value 1
           operator
           value 2      
       
-    calculate on key value pairs in object
+4. Calculate on key value pairs in object
         if statement for operator
 
-    return calculated value
+    
+5. Return calculated value
 
 
 test 1 -> one number is stringified
