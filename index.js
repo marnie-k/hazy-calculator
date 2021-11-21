253db394de3d3ae49dcee5b58768ede4079a3768
Marnie K <marnie.kelly.123@gmail.com>
2021-11-20 19:15:14 -0500

    brainstorming problem solving

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ an  array that can contain numbers, strings, empty strings, null, zeros
 3. What is the output?
 a number or NaN
 
+for all tests:
+for loop??? beecause of array
+variable a defined
+operator defined
+variable b defined
+
+stringified numbers -> converted to numbers
+Null -> converted to zero
+non-numeric strings -> ignored
+operator -> identified and operation assigned
+
+
 test 1 -> one number is stringified
 
 test 2 -> null === 0
 
-test 3 ->  ignore eempty strings and continue operation
+test 3 ->  ignore empty strings and continue operation
 
 test 4 -> ignore non-numeric strings and continue operation
 
