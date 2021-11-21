d0ab448bf9e5e758b06673a19a134f7830d65813
Marnie K <marnie.kelly.123@gmail.com>
2021-11-20 19:03:37 -0500

    adding pseudocode

---
+++ index.js
@@ -0,0 +1,26 @@
+const calculate = () => {
+  /*
+1. What does it do?
+runs an arithmetic calculation based upon an array of inputs 
+(eg. `[2, '*', 10]`) 
+
+2. What  are the inputs (parameters)?
+an  array that can contain numbers, strings, empty strings, null, zeros
+
+3. What is the output?
+a number or NaN
+
+test 1 -> one number is stringified
+
+test 2 -> null === 0
+
+test 3 ->  ignore eempty strings and continue operation
+
+test 4 -> ignore non-numeric strings and continue operation
+
+test 5 -> return NaN if no operator
+
+*/
+}
+
+module.exports = calculate
